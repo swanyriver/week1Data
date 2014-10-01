5398447933d1c1e5c767e573f9b0ab8f6918f23a
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-09-30 21:43:10 -0700

    all exerceses completed, must now debug

--- Q5.c
+++ Q5.c
@@ -6,6 +6,8 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 /*converts ch to upper case, assuming it is in lower case currently*/
 char toUpperCase(char ch){
@@ -19,14 +21,28 @@ char toLowerCase(char ch){
 
 void sticky(char* word){
      /*Convert to sticky caps*/
+    bool up = true;
+    for(int i=0; word[i]!='\0';i++){
+        if(up) word[i] = toupper(word[i]);
+        else word[i] = tolower(word[i]);
+
+        up=!up;
+    }
 }
+
+
 
 int main(){
     /*Read word from the keyboard using scanf*/
-    
+    char str[31];
+    printf("please enter a word (30chars Max): ");
+    scanf("%30s",str);
+
     /*Call sticky*/
+    sticky(str);
     
     /*Print the new word*/
+    printf("\n%s\n\n",str);
     
     return 0;
 }
