e61a736a5e416c3da6becb9ae0c4f23ffad58a6f
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-09-30 18:49:42 -0700

    q0 done gitignore created

--- Q0.c
+++ Q0.c
@@ -8,22 +8,33 @@
 #include <stdlib.h>
 
 void fooA(int* iptr){
-     /*Print the value pointed to by iptr*/
-     
-     /*Print the address pointed to by iptr*/
-     
-     /*Print the address of iptr itself*/
+    /*Print the value pointed to by iptr*/
+    printf("%d\n",*iptr);
+
+
+    /*Print the address pointed to by iptr*/
+    printf("%x\n",iptr);
+
+    /*Print the address of iptr itself*/
+    printf("%x\n", &iptr);
+
 }
 
 int main(){
     
+    const int VALUE = 10;
+
     /*declare an integer x*/
-    
+    int x=VALUE;
+
     /*print the address of x*/
-    
+    printf("%x\n", &x);        //todo print in int or hex?
+
     /*Call fooA() with the address of x*/
-    
+    fooA(&x);
+
     /*print the value of x*/
+    printf("%d\n",x);
     
     return 0;
 }
